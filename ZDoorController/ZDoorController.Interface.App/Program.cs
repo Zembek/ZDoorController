add70543c1048836dd2d6a1f8364417b6ed70898
Łukasz Ziemblicki <lzz1990@tlen.pl>
2024-09-22 17:04:40 +0200

    setup standard rpi media packages

--- ZDoorController/ZDoorController.Interface.App/Program.cs
+++ ZDoorController/ZDoorController.Interface.App/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Iot.Device.Media;
+
+#if DEBUG
+Console.WriteLine("Press any key for debug");
+Console.ReadKey();
+#endif
 Console.WriteLine("Hello, World!");
+
+VideoConnectionSettings settings = new VideoConnectionSettings(busId: 0, captureSize: (2592, 1944), pixelFormat: VideoPixelFormat.JPEG);
+
+using VideoDevice device = VideoDevice.Create(settings);
+
+IEnumerable<VideoPixelFormat> formats = device.GetSupportedPixelFormats();
+
+foreach (var format in formats)
+{
+    Console.WriteLine($"Pixel Format {format}");
+    IEnumerable<Resolution> resolutions = device.GetPixelFormatResolutions(format);
+    if (resolutions is not null)
+    {
+        foreach (var res in resolutions)
+        {
+            Console.WriteLine($"   min res: {res.MinWidth} x {res.MinHeight} ");
+            Console.WriteLine($"   max res: {res.MaxWidth} x {res.MaxHeight} ");
+        }
+    }
+}
