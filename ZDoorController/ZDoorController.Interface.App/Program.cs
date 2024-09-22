1b76b7f8b6b1cb1260d843faa0964d6766f21907
Łukasz Ziemblicki <lzz1990@tlen.pl>
2024-09-22 17:28:44 +0200

    try to capture photo

--- ZDoorController/ZDoorController.Interface.App/Program.cs
+++ ZDoorController/ZDoorController.Interface.App/Program.cs
@@ -9,19 +9,20 @@ Console.WriteLine("Hello, World!");
 VideoConnectionSettings settings = new VideoConnectionSettings(busId: 0, captureSize: (2592, 1944), pixelFormat: VideoPixelFormat.JPEG);
 
 using VideoDevice device = VideoDevice.Create(settings);
+device.Capture("/home/pi/Pictures/capture.jpg");
 
-IEnumerable<VideoPixelFormat> formats = device.GetSupportedPixelFormats();
+//IEnumerable<VideoPixelFormat> formats = device.GetSupportedPixelFormats();
 
-foreach (var format in formats)
-{
-    Console.WriteLine($"Pixel Format {format}");
-    IEnumerable<Resolution> resolutions = device.GetPixelFormatResolutions(format);
-    if (resolutions is not null)
-    {
-        foreach (var res in resolutions)
-        {
-            Console.WriteLine($"   min res: {res.MinWidth} x {res.MinHeight} ");
-            Console.WriteLine($"   max res: {res.MaxWidth} x {res.MaxHeight} ");
-        }
-    }
-}
+//foreach (var format in formats)
+//{
+//    Console.WriteLine($"Pixel Format {format}");
+//    IEnumerable<Resolution> resolutions = device.GetPixelFormatResolutions(format);
+//    if (resolutions is not null)
+//    {
+//        foreach (var res in resolutions)
+//        {
+//            Console.WriteLine($"   min res: {res.MinWidth} x {res.MinHeight} ");
+//            Console.WriteLine($"   max res: {res.MaxWidth} x {res.MaxHeight} ");
+//        }
+//    }
+//}
