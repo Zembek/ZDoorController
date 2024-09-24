6e35b18bd019d763de69721628006cf92ab5837a
Łukasz Ziemblicki <lzz1990@tlen.pl>
2024-09-24 20:10:52 +0200

    fix config, improve appserivce

--- ZDoorController/ZDoorController.Interface.App/ApplicationService.cs
+++ ZDoorController/ZDoorController.Interface.App/ApplicationService.cs
@@ -5,17 +5,22 @@ using ZDoorController.Interface.App.Modules.Interfaces;
 
 namespace ZDoorController.Interface.App
 {
-    public class ApplicationService : IHostedService, IDisposable
+    public class ApplicationService : IHostedService, IHostedLifecycleService, IDisposable
     {
         private readonly IPhotoModule _photoModule;
         private readonly IButtonModule _buttonModule;
         private readonly IRelayModule _relayModule;
 
-        public ApplicationService(IPhotoModule photoModule, IButtonModule buttonModule, IRelayModule relayModule)
+        public ApplicationService(IHostApplicationLifetime appLifetime, IPhotoModule photoModule, IButtonModule buttonModule, IRelayModule relayModule)
         {
             _photoModule = photoModule;
             _buttonModule = buttonModule;
             _relayModule = relayModule;
+
+
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopping.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
         }
 
         public void Dispose()
@@ -46,6 +51,28 @@ namespace ZDoorController.Interface.App
             return Task.CompletedTask;
         }
 
+        public Task StartedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StartingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
         public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StoppedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StoppingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        private void OnStarted()
+        {
+            Console.WriteLine("OnStarted has been called.");
+        }
+
+        private void OnStopping()
+        {
+            Console.WriteLine("OnStopping has been called.");
+        }
+        private void OnStopped()
+        {
+            Console.WriteLine("OnStopped has been called.");
+        }
     }
 }
