9ca3ea2528ec295a3b9e9f5322cd9f1bf7d54dc0
Łukasz Ziemblicki <lzz1990@tlen.pl>
2024-10-09 15:04:33 +0200

    draw pikachu

--- ZDoorController/ZDoorController.Interface.App/ApplicationService.cs
+++ ZDoorController/ZDoorController.Interface.App/ApplicationService.cs
@@ -51,15 +51,15 @@ namespace ZDoorController.Interface.App
             RunApp = true;
             while (RunApp)
             {
-                //_displayManager.DrawPikachu(0,0);
-                await CheckButtons();
+                _displayManager.DrawPikachu(0,0);
+                //await CheckButtons();
 
-                _displayManager.Clear();
+                //_displayManager.Clear();
 
-                GetTemperature();
-                GetReedSwitch();
+                //GetTemperature();
+                //GetReedSwitch();
 
-                _displayManager.Update();
+                _displayManager.Update(true);
 
                 Thread.Sleep(500);
             }
